9970aafdd88945f8839bf0e185d7d776383d7d00
Sophie van der Burg <sophievdburg9@gmail.com>
2022-03-18 11:10:56 +0100

    removed some notes

--- scripts/modules/zoekfunctie.js
+++ scripts/modules/zoekfunctie.js
@@ -5,7 +5,6 @@ export const h3 = document.querySelector("main div:first-of-type")
 
 export function zoeken (){
     getData()
-    // h3.insertAdjacentElement('afterbegin', `${query}`)
 }
 
 zoekBalk.addEventListener("keydown", (e) => {
